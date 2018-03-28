4f2e7ab3aa9386d760cdaf33f47ad1572e1a5ad8
Otto K. von Sperling <ottok92@gmail.com>
2018-03-28 00:03:50 -0300

    First commit. Version 4.0

---
+++ LAB1.cpp
@@ -0,0 +1,115 @@
+// Otto Kristian von Sperling
+// MAT: 12/0131510
+// Lucas M. L.
+//MAT: xx/xxxxxxx
+//###############################################################
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <utility>
+#include <algorithm>
+#include "network.hpp"	// My rewritten version of it. New parser and all
+
+typedef std::vector< std::list<int> > adjList; //Adjacency list disregarding the edge's weight
+// In this program, 2 types of djacency list will be used.
+// The simple one being a vector of lists of ints std::vector<list<pair<int, int>>>
+// The complex one being a vector of struct VERTEX due to the likely use of weighted graphs in future projects.
+
+//######################## DEFINING FUNCTIONS ####################
+adjList buildAdjacencyList(std::vector<VERTEX> &sourceGraph);
+void printAdjacencyList(adjList &sourceToPrint);
+void printMaxToMinDegree(std::vector<VERTEX> &adjListSource);
+std::vector<VERTEX> sortMaxToMin(std::vector<VERTEX> &source);
+
+// ###################### MAIN ###################################
+int main(int argc, char** argv)
+{
+	adjList adjacencyList;
+	NETWORK graph("karate.gml");	
+	
+	adjacencyList = buildAdjacencyList((graph.vertex));
+	printAdjacencyList(adjacencyList);
+//	printMaxToMinDegree(graph.vertex);
+	return 0;
+}
+
+// ######################## FUNCTIONS ############################
+ adjList buildAdjacencyList(std::vector<VERTEX> &sourceGraph)
+{
+	try
+	{
+		adjList newAdjList((sourceGraph.size()));
+
+		for(std::vector<VERTEX>::iterator it = sourceGraph.begin(); it != sourceGraph.end(); ++it) // goes through the vector for each vertex
+		{
+
+			for(std::list< std::pair< int, int > >::iterator jt = (*it).edge.begin(); jt != (*it).edge.end(); ++jt) // goes through the list for each edge
+			{
+				newAdjList[((*it).id) - 1].push_back((*jt).first);	//makes first link	v1 --> v2
+				newAdjList[((*jt).first) - 1].push_back((*it).id);	//makes second link	v2 --> v1
+			}
+		}
+		return newAdjList;
+	}
+	catch(std::exception e)
+	{
+		throw e.what();
+	}
+}
+
+void printAdjacencyList(adjList &sourceToPrint)
+{
+	for(unsigned int it = 0; it < sourceToPrint.size(); ++it)
+	{
+		std::cout << "VERTEX " << (it + 1) << std::endl;
+		for(std::list<int>::iterator jt = sourceToPrint[it].begin(); jt != sourceToPrint[it].end(); ++jt)
+		{
+			std::cout << "\tEDGE TO " << (*jt) << std::endl;
+		}
+	}
+}
+
+std::vector<VERTEX> sortMaxToMin(std::vector<VERTEX> &source)
+{
+	std::vector<VERTEX> middleMan = source;
+	std::vector<VERTEX>::iterator it = middleMan.begin();
+
+	try
+	{
+		std::sort(middleMan.begin(), middleMan.end(), [](const VERTEX& left, const VERTEX& right)
+		{
+			return left.degree > right.degree;	// SORTING THE VECTOR WITH AN IMPLICIT LAMBDA FUNCTION
+		});
+		return middleMan;
+	}
+	catch(std::exception e)
+	{
+		throw e.what();
+	}
+	//returns adjacency list from a sorted set of vector<VERTEX>
+}
+
+void printMaxToMinDegree(std::vector<VERTEX> &adjListSource)
+{
+	std::vector<VERTEX> toPrint = sortMaxToMin(adjListSource);
+	try
+	{
+		for(std::vector<VERTEX>::iterator it = toPrint.begin(); it != toPrint.end(); ++it)
+		{
+			std::cout << "VERTEX " << (*it).id << std::endl;
+			for(std::list< std::pair< int, int > >::iterator jt = (*it).edge.begin(); jt != (*it).edge.end(); ++jt)
+			{
+				std::cout << "\t\tTARGET " << (*jt).first << " | WEIGHT " << (*jt).second << std::endl;
+			}
+			std::cout << std::endl;
+		}
+		toPrint.clear();
+	}
+	catch(std::exception e)
+	{
+		throw e.what();
+	}
+}
+
+void findMaximalClique(std::vector<int> R, std::vector<int> P, std::vector<int> X);
